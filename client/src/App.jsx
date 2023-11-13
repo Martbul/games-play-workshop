4b48f753fa5fddd902a2a7c7bc4956e11b28c70e
Martbul <martbul01@gmail.com>
2023-11-13 19:43:30 +0200

    add gameList page and reat-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import Header from "./components/Header/Header"
+import Home from "./components/home/Home"
+import GameList from "./components/game-list/GameList"
+import {Routes,Route} from "react-router-dom"
 
 function App() {
   
   return (
     <div id='box'>
     <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/games" element={<GameList/>}/>
+    </Routes>
+    
     </div>
    
   )
