6842b773412899599f256924d9382a35998a6696
Martbul <martbul01@gmail.com>
2023-11-20 18:43:17 +0200

    create custom hook
    login is now controlled form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,20 +1,43 @@
-export default function Login(){
-    return( <section id="login-page" className="auth">
-    <form id="login">
+import useForm from "../../hooks/useForm";
 
+export default function Login() {
+  const { formValues, onChange, onSubmit } = useForm({
+    email:'',
+    password:'',
+  });
+
+  return (
+    <section id="login-page" className="auth">
+      <form id="login" onSubmit={onSubmit}>
         <div className="container">
-            <div className="brand-logo"></div>
-            <h1>Login</h1>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Sokka@gmail.com"/>
+          <div className="brand-logo"></div>
+          <h1>Login</h1>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={formValues.email}
+          />
 
-            <label htmlFor="login-pass">Password:</label>
-            <input type="password" id="login-password" name="password"/>
-            <input type="submit" className="btn submit" value="Login"/>
-            <p className="field">
-                <span>If you don't have profile click <a href="#">here</a></span>
-            </p>
+          <label htmlFor="login-pass">Password:</label>
+          <input
+            type="password"
+            id="login-password"
+            name="password"
+            onChange={onChange}
+            value={formValues.password}
+          />
+          <input type="submit" className="btn submit" value="Login" />
+          <p className="field">
+            <span>
+              If you don't have profile click <a href="#">here</a>
+            </span>
+          </p>
         </div>
-    </form>
-</section>)
+      </form>
+    </section>
+  );
 }
